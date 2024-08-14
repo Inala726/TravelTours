47bc42ff97b543b1877d1a119880e652c6fa2add
Inala <inalajacob740@gmail.com>
2024-08-14 09:33:06 +0100

    Second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,30 @@
 // import React from 'react'
 
 import { FaSquarespace } from "react-icons/fa"
+import { FaArrowRight } from "react-icons/fa6"
 
 const Navbar = () => {
   return (
     <>
-        <nav className="flex items-center justify-between p-4">
+        <nav className="flex items-center justify-between px-20 py-8">
             <a href="">
             <div className="flex items-center gap-2">
-              <FaSquarespace size={25}/>
+              <FaSquarespace size={25} className="text-[#f98939]"/>
               <p className="text-xl font-bold text-blue-950">CoTR</p>
             </div>
             </a>
             <div className="">
-              <ul className="flex gap-10 text-xl">
+              <ul className="flex gap-[60px] text-[16px] text-[#9d97a7]">
                 <li><a href="">Home</a></li>
                 <li><a href="">Contact</a></li>
                 <li><a href="">About</a></li>
+                <li><a href="">Services</a></li>
               </ul>
             </div>
-            <div className=""></div>
+            <div className="flex items-center gap-1 text-[#f96f39]">
+              Book 
+              <FaArrowRight />
+            </div>
         </nav>
     </>
   )
